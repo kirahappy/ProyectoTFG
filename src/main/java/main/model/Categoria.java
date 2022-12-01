a953a2d27303da13bd675c7611882f1ba107f474
MariaSuarez <rfb52455@educastur.es>
2022-12-01 12:33:44 +0100

    Declaración atributos y clumnas de la clase Categoria (se va a borrar)

--- src/main/java/main/model/Categoria.java
+++ src/main/java/main/model/Categoria.java
@@ -1,5 +1,26 @@
 package main.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Categorias")
 public class Categoria {
 
+	@Column(name="amor")
+	String amor;
+	
+	@Column(name="amistad")
+	String amistad;
+	
+	@Column(name="familia")
+	String familia;
+	
+	@Column(name="trabajo")
+	String trabajo;
+	
+	@Column(name="estudios")
+	String estudios;
+	
 }

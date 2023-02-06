a26d12eb559c49ec708e18e69e3fbd41755f3af4
MariaSuarez <rfb52455@educastur.es>
2023-02-06 10:07:26 +0100

    Añadiendo métodos a MentiraReporitory

--- src/main/java/lieTime/repository/MentiraRepository.java
+++ src/main/java/lieTime/repository/MentiraRepository.java
@@ -1,5 +1,7 @@
 package lieTime.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,12 @@ import lieTime.dao.MentiraDao;
 
 @Repository
 public interface MentiraRepository extends JpaRepository<MentiraDao, Integer> {
-	
+	public MentiraDao insertarMentira(MentiraDao mentira);
+
+	public List<MentiraDao> listarMentiras();
+
+	public MentiraDao obternerMentiraId(Integer id);
+
+	public void deleteMentiraId(Integer id);
 
 }

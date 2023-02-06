c9f37f2d823202dbf7a8650f0145d75e47cebd1f
MariaSuarez <rfb52455@educastur.es>
2023-02-06 09:43:28 +0100

    Crear ComentarioRepository

---
+++ src/main/java/lieTime/repository/ComentarioRepository.java
@@ -0,0 +1,14 @@
+package lieTime.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import lieTime.dao.ComentarioDao;
+
+public interface ComentarioRepository extends JpaRepository<ComentarioDao, Integer> {
+	public ComentarioDao insertarComentario (ComentarioDao comentario);
+	public List<ComentarioDao> mostrarComentarios();
+	public ComentarioDao eliminarComentarioId(Integer id);
+
+}

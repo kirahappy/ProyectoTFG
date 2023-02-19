005e9e81de3093525e1e51a2b445d9284f3706c6
MariaSuarez <rfb52455@educastur.es>
2023-02-19 21:53:31 +0100

    Solucionando error de re-run

--- src/main/java/lieTime/dao/MentiraDao.java
+++ src/main/java/lieTime/dao/MentiraDao.java
@@ -2,6 +2,7 @@
 package lieTime.dao;
 
 import java.util.HashSet;
+
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -14,12 +15,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lieTime.model.Categoria;
 
--- src/main/java/lieTime/spring/Config.java
+++ src/main/java/lieTime/spring/Config.java
@@ -1,17 +1,17 @@
-package lieTime.spring;
-
-import org.springframework.context.annotation.Bean;
-
-import org.springframework.context.annotation.Configuration;
-
-import lieTime.repository.MentiraRepository;
-import lieTime.service.MentiraService;
-
-@Configuration
-public class Config {
-
-	@Bean
-	public MentiraService mentiraService(MentiraRepository mentiraRepository) {
-		return new MentiraService(mentiraRepository);
-	}
-}
+//package lieTime.spring;
+//
+//import org.springframework.context.annotation.Bean;
+//
+//import org.springframework.context.annotation.Configuration;
+//
+//import lieTime.repository.MentiraRepository;
+//import lieTime.service.MentiraService;
+//
+//@Configuration
+//public class Config {
+//
+//	@Bean
+//	public MentiraService mentiraService(MentiraRepository mentiraRepository) {
+//		return new MentiraService(mentiraRepository);
+//	}
+//}
